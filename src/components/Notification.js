5a09c60e0a807b814f3f8a38f9e59c7bf5d91bfc
idrissiradi <idrissiradi@gmail.com>
2020-08-08 15:33:47 +0100

    first commit

---
+++ src/components/Notification.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Notification = ({ showNotification }) => {
+  return (
+    <div className={`notification-container ${showNotification ? 'show' : ''}`}>
+      <p>You have already entered this letter</p>
+    </div>
+  )
+}
+
+export default Notification
