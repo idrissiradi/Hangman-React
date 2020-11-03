bb1a4d27cb2fd44aac334fa33dc8150f2d51f999
idrissiradi <idrissiradi@gmail.com>
2020-11-03 14:25:52 +0100

    last commit

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -6,10 +6,10 @@ const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable, playAg
     let finalMessageRevealWord = '';
     let playable = true;
 
-    if( checkWin(correctLetters, wrongLetters, selectedWord) == 'win') {
+    if( checkWin(correctLetters, wrongLetters, selectedWord) === 'win') {
         finalMessage = 'Congratulations! You won! 😃';
         playable = false;
-    } else if ( checkWin(correctLetters, wrongLetters, selectedWord) == 'lose') {
+    } else if ( checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
         finalMessage = 'Unfortunately you lost. 😕';
         finalMessageRevealWord = `...the word was: ${selectedWord}`;
         playable = false;
@@ -18,7 +18,7 @@ const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable, playAg
     useEffect(() => setPlayable(playable));
 
     return (
-        <div className="popup-container" style={finalMessage != '' ? {display: 'flex'} : {}}>
+        <div className="popup-container" style={finalMessage !== '' ? {display: 'flex'} : {}}>
             <div className="popup">
             <h2>{finalMessage}</h2>
             <h3>{finalMessageRevealWord}</h3>
